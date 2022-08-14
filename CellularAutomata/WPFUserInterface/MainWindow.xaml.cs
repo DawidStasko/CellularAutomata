e2da5c12bce0e6d31ff0c6e3dde370a81a1b1fbf
Dawid Staśko <dawidstasko@outlook.com>
2022-08-15 01:48:16 +0200

    Add possibility to draw simple 2D board with rectangle cells.

--- CellularAutomata/WPFUserInterface/MainWindow.xaml.cs
+++ CellularAutomata/WPFUserInterface/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,17 +15,86 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WPFUserInterface.Annotations;
 
 namespace WPFUserInterface
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        private ObservableCollection<Cell> _cells;
+
+        public ObservableCollection<Cell> Cells
+        {
+            get => _cells;
+            set
+            {
+                _cells = value; 
+                OnPropertyChanged();
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            WidthInput.Text = "10";
+            HeightInput.Text = "10";
+            CellSizeInput.Text = "15";
+            Cells = new ObservableCollection<Cell>();
+            DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void DrawBoard(int width, int height, int cellSize)
+        {
+            Cells.Clear();
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    var position = new Point(j * cellSize + 1.0, i * cellSize + 1.0);
+                    Cells.Add(new(position, cellSize));
+                }
+            }
+        }
+
+        public void DrawBoardCommand(object sender, RoutedEventArgs routedEventArgs)
+        {
+            Int32.TryParse(WidthInput.Text, out int width);
+            Int32.TryParse(HeightInput.Text, out int height);
+            Int32.TryParse(CellSizeInput.Text, out int size);
+            DrawBoard(width, height, size);
+
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class Cell:INotifyPropertyChanged
+    {
+        public int Size { get; }
+
+        public Point Position { get; }
+
+        public Cell(Point position, int size)
+        {
+            Position = position;
+            Size = size;
+        }
+        
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
