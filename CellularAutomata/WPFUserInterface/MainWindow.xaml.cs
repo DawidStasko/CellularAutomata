41fe67ab63ab51d18998d0f60a2b9d819f829bae
DawidStasko <stasko.dawid@gmail.com>
2022-10-06 15:07:36 +0200

    Cleanup.

--- CellularAutomata/WPFUserInterface/MainWindow.xaml.cs
+++ CellularAutomata/WPFUserInterface/MainWindow.xaml.cs
@@ -1,24 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Globalization;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Microsoft.Xaml.Behaviors.Core;
-using WPFUserInterface.Annotations;
-using WPFUserInterface.Domain;
+﻿using System.Windows;
 using WPFUserInterface.ViewModels;
 
 namespace WPFUserInterface
